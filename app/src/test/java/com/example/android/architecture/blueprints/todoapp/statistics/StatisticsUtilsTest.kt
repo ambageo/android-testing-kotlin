28c56333894557b4ef7fb03a2a789430a273b0ac
GeorgeAmpartzidis <george.ampartzidis@mpway.com>
2021-07-01 11:20:05 +0100

    Write test for getActiveAndCompletedStats

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -0,0 +1,18 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.junit.Assert.*
+import org.junit.Test
+
+class StatisticsUtilsTest {
+
+    @Test
+    fun getActiveAndCompletedStats_noCompleted_returnsHundredZero(){
+        val tasks = listOf<Task>(
+            Task("title", "description", false)
+        )
+        val result = getActiveAndCompletedStats(tasks)
+        assertEquals(result.activeTasksPercent, 100f)
+        assertEquals(result.completedTasksPercent, 0f)
+    }
+}
