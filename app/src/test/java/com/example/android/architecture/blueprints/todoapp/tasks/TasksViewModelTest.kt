b09acae3fbed57c5565f97a1b49c22ab4b97edd0
GeorgeAmpartzidis <george.ampartzidis@mpway.com>
2021-07-02 09:42:43 +0100

    AddedAndroidX, AndroidJUnit4 test runner, Robolectric ans @get:Rule

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,8 +1,13 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
+import android.service.autofill.Validators.not
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
+import org.hamcrest.CoreMatchers.nullValue
 import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -11,6 +16,14 @@ class TasksViewModelTest {
 
     @Test
     fun addNewTask_setsNewTask() {
+
+        /*
+        * This rule runs all architecture components related background jobs in the same thread,
+        * ensuring that the test results happen synchronously and in a repeatable order
+        */
+        @get:Rule
+        var instantTaskExecutorRule = InstantTaskExecutorRule()
+
         // Given a fresh TasksViewModel
         // In this particular case we need an application (the ViewModel is an AndroidViewModel),
         // otherwise we wouldn't need is (if it were just a ViewModel)
@@ -19,7 +32,8 @@ class TasksViewModelTest {
         // When adding a new task
         tasksViewModel.addNewTask()
 
-
         // Then the new task event is triggered
+        val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
+        assertThat(value.getContentIfNotHandled(), not(nullValue()))
     }
 }
