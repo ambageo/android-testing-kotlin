f2bd18f49fde2ebaff4e2d639387f96c5e41f198
GeorgeAmpartzidis <george.ampartzidis@mpway.com>
2021-07-05 12:24:30 +0100

    Refactored to use FakeRepository inside ViewModel

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,8 +1,11 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
+import android.app.Application
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeTestRepository
 import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
 import org.hamcrest.CoreMatchers.*
 import org.hamcrest.MatcherAssert.assertThat
@@ -12,10 +15,13 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
+//@RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
 
     private lateinit var tasksViewModel: TasksViewModel
+
+    // Use a fake repository to be injected into the viewModel
+    private lateinit var tasksRepository: FakeTestRepository
 
     /*
     * This rule runs all architecture components related background jobs in the same thread,
@@ -27,7 +33,17 @@ class TasksViewModelTest {
     // Since we need a TaskViewModel shared between all tests, we can use @Before to set it
     @Before
     fun setUpViewModel() {
-        tasksViewModel = TasksViewModel((ApplicationProvider.getApplicationContext()))
+        // We initialise the tasks to 3, with one active and two completed
+        tasksRepository = FakeTestRepository()
+        val task1 = Task("Title1", "Description1")
+        val task2 = Task("Title2", "Description2", true)
+        val task3 = Task("Title3", "Description3", true)
+
+        tasksRepository.addTasks(task1, task2, task3)
+
+        tasksViewModel = TasksViewModel(tasksRepository)
+        // No need to use AndroidX Test, so we can remove the @RunWith(AndroidJUnit4::class)
+        //tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
     }
     @Test
     fun addNewTask_setsNewTask() {
