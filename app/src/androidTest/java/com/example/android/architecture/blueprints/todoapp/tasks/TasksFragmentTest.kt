3a5d22d6f7f92f6d302018ae5fb5b97efb0fa015
GeorgeAmpartzidis <george.ampartzidis@mpway.com>
2021-07-06 11:27:03 +0100

    Added Mockito test for TasksFragment to TaskDetailFragment navigation

---
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksFragmentTest.kt
@@ -0,0 +1,72 @@
+package com.example.android.architecture.blueprints.todoapp.tasks
+
+import android.os.Bundle
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import com.example.android.architecture.blueprints.todoapp.R
+import com.example.android.architecture.blueprints.todoapp.ServiceLocator
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeAndroidTestRepository
+import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+
+@RunWith(AndroidJUnit4::class)
+@MediumTest
+@ExperimentalCoroutinesApi
+class TasksFragmentTest {
+    private lateinit var repository: TasksRepository
+
+    // Before each test, we are swapping with a fake test repository
+    @Before
+    fun initRepository(){
+        repository = FakeAndroidTestRepository()
+        ServiceLocator.tasksRepository = repository
+    }
+    @After
+    fun cleanupDb() = runBlockingTest{
+        ServiceLocator.resetRepository()
+    }
+
+    @Test
+    fun clickTask_navigateToDetailFragment() = runBlockingTest {
+        repository.saveTask(Task("Task 1", "Description 1", false, "id1"))
+        repository.saveTask(Task("Task 2", "Description 2", true, "id2"))
+
+        // GIVEN - On the home screen
+
+        // Launch the fragment
+        val scenario = launchFragmentInContainer<TasksFragment>(Bundle(), R.style.AppTheme)
+
+        val navController = mock(NavController::class.java)
+        // Pass the mock as the fragment's NavController
+        scenario.onFragment {
+            Navigation.setViewNavController(it.view!!, navController)
+        }
+
+        // WHEN - Click on the first list item
+        onView(withId(R.id.tasks_list))
+            .perform(RecyclerViewActions.actionOnItem<RecyclerView.ViewHolder>(
+                hasDescendant(withText("Task 1")), click()))
+
+            // THEN - verify that we navigate to the detail screen
+        verify(navController).navigate(
+            TasksFragmentDirections.actionTasksFragmentToTaskDetailFragment("id1")
+        )
+    }
+}
